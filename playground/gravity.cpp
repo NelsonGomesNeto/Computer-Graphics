bc4a43c38b8faee51706b7057ff08cc3842ead61
NelsonGomesNeto <ngn@ic.ufal.br>
2018-12-02 17:52:41 -0300

    Adds simple solar system ^~^

--- playground/gravity.cpp
+++ playground/gravity.cpp
@@ -1,19 +1,92 @@
 #ifndef _WIN32
   #include <windows.h>
 #endif
+#include <vector>
+#include <string>
+using namespace std;
 #include <math.h>
 #include <GL/freeglut.h>
 const int width = 800, height = 800;
-const double pi = acos(-1), tau = 2 * acos(-1), gravitationConstant = 100;
+const double pi = acos(-1), tau = 2 * acos(-1), gravitationConstant = 1;
 double degToRad(double a) { return(a*pi/180.0); }
-struct Point { double x, y; };
+struct Point
+{
+  double x, y;
+  Point operator+(const Point &a) { return(Point({x + a.x, y + a.y})); }
+  Point operator-(const Point &a) { return(Point({x - a.x, y - a.y})); }
+  Point operator-() { return(Point({-x, -y})); }
+  Point operator*(const double a) { return(Point({x * a, y * a})); }
+  Point operator/(const double a) { return(Point({x / a, y / a})); }
+  void operator+=(const Point &a) { x += a.x, y += a.y; }
+};
+double distance(Point a, Point b) { return(sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2))); }
 struct Object { Point position, speed, acceleration; double mass; };
-Object sun = {{400, 400}, {0, 0}, {0, 0}, 50}, earth = {{200, 400}, {0, 50}, {0, 0}, 1};
+Point mouse;
+Object sun = {{400, 400}, {0, 0}, {0, 0}, 1000};
+vector<Object> planets;
+bool addingPlanet = false; Point startingPoint;
+
+void printText(string s, double x, double y)
+{
+  glRasterPos2d(x, y);
+  for (int i = 0; i < s.size(); i++) glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, s[i]);
+  glRasterPos2d(-x, -y);
+}
+
+void passiveMotionHandler(int x, int y)
+{
+  mouse.x = x, mouse.y = height - y;
+}
+
+void keyboardHandler(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
+    case '1': sun.mass += 10;
+      break;
+    case '2': sun.mass -= 10;
+      break;
+    case 'a':
+      if (!addingPlanet) startingPoint = {(double) x, (double) height - y};
+      else planets.push_back({startingPoint, (startingPoint - Point({(double) x, (double) height - y})) / 10, {0, 0}, 1});
+      addingPlanet = !addingPlanet;
+      break;
+    case 's':
+      planets.pop_back();
+      break;
+    default:
+      break;
+  }
+}
 
 void scheduleUpdate(int v)
 {
   glutTimerFunc(10, scheduleUpdate, 1);
-  earth.acceleration.x = earth.speed.x * earth.speed.mass
+  // ||d|| = d / sqrt(d.x^2 + d.y^2)
+  // Gravitational Force = G * m1 * m2 / d^2 * ||d|| = G * m1 * m2 / d^1.5 * unitary(d.x, d.y)
+  // Force = mass * acceleration
+  // acceleration = Force / mass
+
+  sun.acceleration = {0, 0};
+  for (int i = 0; i < planets.size(); i ++)
+  {
+    double d = distance(planets[i].position, sun.position);
+    double force = gravitationConstant * planets[i].mass * sun.mass / pow(d, 1.5);
+    planets[i].acceleration = ((sun.position - planets[i].position) / d) * force / planets[i].mass;
+    sun.acceleration += ((planets[i].position - sun.position) / d) * force / sun.mass;
+    for (int j = 0; j < planets.size(); j ++)
+    {
+      if (i == j) continue;
+      d = distance(planets[i].position, planets[j].position);
+      force = gravitationConstant * planets[i].mass * planets[j].mass / pow(d, 1.5);
+      planets[i].acceleration += ((planets[j].position - planets[i].position) / d) * force / planets[i].mass;
+    }
+    planets[i].speed += planets[i].acceleration;
+    planets[i].position += planets[i].speed;
+  }
+  sun.speed += sun.acceleration;
+  sun.position += sun.speed;
+
   glutPostRedisplay();
 }
 
@@ -30,8 +103,22 @@ void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  drawCircle(sun.position, sun.mass, 10);
-  drawCircle(earth.position, earth.mass, 10);
+  // printText("sun.mass: " + to_string(sunAux.mass) + " earth.position.x: " + to_string(earthAux.position.x) + " earth.speed.y: " + to_string(earthAux.speed.y), 0, height - 100);
+  printText("planets: " + to_string(planets.size()), 0, height - 50);
+ 
+  drawCircle(sun.position, 50, 10);
+  for (Object planet: planets) drawCircle(planet.position, 10, 10);
+
+  if (addingPlanet)
+  {
+    drawCircle(startingPoint, 10, 10);
+    glBegin(GL_LINE);
+      glColor3ub(255, 255, 255);
+      glVertex2d(startingPoint.x, startingPoint.y);
+      glVertex2d(mouse.x, mouse.y);
+    glEnd();
+    printText("(" + to_string((startingPoint.x - mouse.x) / 10) + ", " + to_string((startingPoint.y - mouse.y) / 10) + ")", (mouse.x + startingPoint.x) / 2.0, (mouse.y + startingPoint.y) / 2.0);
+  }
 
   glFlush();
 }
@@ -51,5 +138,7 @@ int main(int argc, char **argv)
     init();
   glutDisplayFunc(display);
   glutTimerFunc(10, scheduleUpdate, 1);
+  glutKeyboardFunc(keyboardHandler);
+  glutPassiveMotionFunc(passiveMotionHandler);
   glutMainLoop();
 }
