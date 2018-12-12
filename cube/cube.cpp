bfe5f4d61833962472cc53ee4a2548d21993dd63
NelsonGomesNeto <ngn@ic.ufal.br>
2018-12-12 12:50:38 -0300

    Adds rotation to cube, YaY \o/

--- cube/cube.cpp
+++ cube/cube.cpp
@@ -10,8 +10,22 @@
 using namespace std;
 
 const int width = 600, height = 600;
-double xOffset = 0, yOffset = 0, zOffset = 2.0, cubeSize = 1;
+double xOffset = 0, yOffset = 0, zOffset = 2.0, xAngle = 0, yAngle = 0, zAngle = 0, cubeSize = 1;
 Cube cube;
+
+struct Point { int x, y; };
+Point mouse, startMouse; bool started = false; int axes;
+void passiveMotionHandler(int x, int y)
+{
+  int diff = y - startMouse.y;
+  if (started) switch (axes)
+  {
+    case 1: xAngle = diff; break;
+    case 2: yAngle = diff; break;
+    case 3: zAngle = diff; break;
+    default: break;
+  }
+}
 
 void keyboardHandler(unsigned char key, int x, int y)
 {
@@ -23,6 +37,9 @@ void keyboardHandler(unsigned char key, int x, int y)
     case 'X': xOffset -= 0.05; break;
     case 'y': yOffset += 0.05; break;
     case 'Y': yOffset -= 0.05; break;
+    case '1': if (!started) started = true, mouse.x = x, mouse.y = y, axes = key - '0'; else started = false; break;
+    case '2': if (!started) started = true, mouse.x = x, mouse.y = y, axes = key - '0'; else started = false; break;
+    case '3': if (!started) started = true, mouse.x = x, mouse.y = y, axes = key - '0'; else started = false; break;
     default: moveCube(cube, key); break;
   }
 }
@@ -31,6 +48,33 @@ void scheduleUpdate(int value)
 {
   glutTimerFunc(10, scheduleUpdate, 1);
   glutPostRedisplay();
+}
+
+void drawArrow()
+{
+  glBegin(GL_LINES);
+    glVertex3d(0, 0, 0);
+    glVertex3d(3, 0, 0);
+  glEnd();
+}
+void drawSpaceVectors()
+{
+  glLineWidth(8);
+  glPushMatrix();
+    // glTranslated(-8*cubeSize, -5*cubeSize, 0);
+    glColor3ub(255, 0, 0);
+    drawArrow(); // x
+    glPushMatrix();
+      glRotated(90, 0, 0, 1);
+      glColor3ub(0, 255, 0);
+      drawArrow(); // y
+    glPopMatrix();
+    glPushMatrix();
+      glRotated(90, 0, 1, 0);
+      glColor3ub(0, 0, 255);
+      drawArrow(); // z
+    glPopMatrix();
+  glPopMatrix();
 }
 
 void drawPiece(Piece &piece, double size)
@@ -88,19 +132,11 @@ void display()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // glPushMatrix();
-  //   glTranslated(xOffset - 3 * cubeSize, yOffset + 3 * cubeSize, zOffset - n / 2 * cubeSize);
-  //   for (int i = 0; i < 6; i ++)
-  //   {
-  //     glColor3dv(colorMap[i]);
-  //     glTranslated(cubeSize, 0, 0);
-  //     glutSolidCube(cubeSize);
-  //   }
-  // glPopMatrix();
-
   glPushMatrix();
+    glRotated(xAngle, 1, 0, 0); glRotated(yAngle, 0, 1, 0); glRotated(zAngle, 0, 0, 1);
+    glTranslated(-n / 2 * cubeSize,n / 2 * cubeSize,n / 2 * cubeSize);
+
     glColor3ub(0, 0, 255);
-    glTranslated(xOffset - n / 2 * cubeSize, yOffset + n / 2 * cubeSize, zOffset - n / 2 * cubeSize);
     drawCube(cube);
   glPopMatrix();
 
@@ -136,6 +172,7 @@ int main(int argc, char **argv)
   glutTimerFunc(10, scheduleUpdate, 1);
   glutReshapeFunc(reshape);
   glutKeyboardFunc(keyboardHandler);
+  glutPassiveMotionFunc(passiveMotionHandler);
   glutMainLoop();
   return(0);
 }
