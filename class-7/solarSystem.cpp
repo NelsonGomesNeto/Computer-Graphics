7a8bfe6ad262f6b7985f61881d0ba26baee91b67
NelsonGomesNeto <ngn@ic.ufal.br>
2019-03-13 11:04:47 -0300

    Adds LIGHTS to Solar System o/

--- class-7/solarSystem.cpp
+++ class-7/solarSystem.cpp
@@ -1,13 +1,17 @@
 #include <GL/freeglut.h>
+#include <stdio.h>
 
 const int width = 800, height = 800;
 int year = 0, day = 0;
+GLfloat lightSpotCutoff = 10;
+double lx = 0, ly = 0, lz = 0;
 
 void scheduleUpdate(int value)
 {
   glutTimerFunc(10, scheduleUpdate, 1);
   // day += 50;
   // if (day >= 360) year += 1, day %= 360;
+  printf("%8.3lf %8.3lf %8.3lf\n", lx, ly, lz);
   day ++, year ++;
   glutPostRedisplay();
 }
@@ -16,21 +20,17 @@ void keyboardHandler(unsigned char key, int x, int y)
 {
   switch (key)
   {
-    case 'd':
-      day = (day + 10) % 360;
-      glutPostRedisplay();
+    case 'w': lz -= 0.1; break;
+    case 's': lz += 0.1; break;
+    case 'a': lx -= 0.1; break;
+    case 'd': lx += 0.1; break;
+    case 'r': ly += 0.1; break;
+    case 'f': ly -= 0.1; break;
+    case '1':
+      lightSpotCutoff += 1;
       break;
-    case 'D':
-      day = (day - 10) % 360;
-      glutPostRedisplay();
-      break;
-    case 'y':
-      year = (year + 5) % 360;
-      glutPostRedisplay();
-      break;
-    case 'Y':
-      year = (year - 5) % 360;
-      glutPostRedisplay();
+    case '2':
+      lightSpotCutoff -= 1;
       break;
     case 27:
       exit(0);
@@ -40,56 +40,81 @@ void keyboardHandler(unsigned char key, int x, int y)
   }
 }
 
+void lightsSetup()
+{
+  GLfloat materialAmbientAndDiffuse[] = {1, 1, 1, 1};
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, materialAmbientAndDiffuse); // Defines objects reflection to ambient and diffuse light
+  GLfloat materialSpecular[] = {1, 1, 1, 1};
+  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, materialSpecular); // Defines objects reflection to specular light
+  glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 128); // Defines objects level of reflection (0 to 128)
+  GLfloat lightAmbient[] = {0.0, 0.0, 0.0, 1}; glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+  GLfloat lightDiffuse[] = {0.8, 0.8, 0.8, 1}; glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+  GLfloat lightSpecular[] = {1, 1, 1, 1}; glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
+  // glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, lightSpotCutoff);
+  glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.1);
+  GLfloat lightPosition[] = {lx, ly, lz, 1};
+  glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+}
+
 void display()
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  glColor3f(1, 1, 1);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  lightsSetup();
+
+  glPushMatrix();
+    glColor3ub(255, 255, 255);
+    glTranslated(lx, ly, lz);
+    glutSolidSphere(0.05, 100, 100);
+  glPopMatrix();
 
   // Sun
   glPushMatrix();
+    glColor3ub(255, 127, 80);
     glRotatef(year, 1, 0, 0);
     glRotatef(day, 0, 0, 1);
-    glutWireSphere(0.5, 20, 16);
+    glutSolidSphere(0.5, 100, 100);
   glPopMatrix();
 
   // Planet 1
   glPushMatrix();
+    glColor3ub(255, 255, 0);
     glRotatef(year, 0, 1, 0);
     glTranslatef(2, 0, 0);
     glPushMatrix();
       glRotatef(day, 0, 1, 0);
-      glutWireSphere(0.1, 10, 8);
+      glutSolidSphere(0.1, 100, 100);
     glPopMatrix();
 
     glPushMatrix();
       glColor3ub(255, 0, 0);
       glRotatef(1.25*day, 0, 0, 1); // Moon 1
       glTranslatef(0.2, 0, 0);
-      glutWireSphere(0.05, 10, 8);
+      glutSolidSphere(0.05, 100, 100);
     glPopMatrix();
 
     glPushMatrix();
     glColor3ub(0, 255, 0);
       glRotatef(1.5*day, 0, 1, 0); // Moon 2
       glTranslatef(0.4, 0, 0);
-      glutWireSphere(0.025, 10, 8);
+      glutSolidSphere(0.025, 100, 100);
     glPopMatrix();
 
     glPushMatrix();
       glColor3ub(0, 0, 255);
       glRotatef(1.75*day, 0, 1, 1); // Moon 3
       glTranslatef(0.3, 0, 0);
-      glutWireSphere(0.025, 10, 8);
+      glutSolidSphere(0.025, 100, 100);
     glPopMatrix();
   glPopMatrix();
 
   // Planet 2
   glPushMatrix();
-    glColor3ub(255, 255, 255);
+    glColor3ub(0, 255, 255);
     glRotatef(2*year, 0, -1, 0);
     glTranslatef(1, 0, 0);
     glRotatef(2*day, 0, 1, 0);
-    glutWireSphere(0.1, 10, 8);
+    glutSolidSphere(0.1, 10, 8);
   glPopMatrix();
 
   glutSwapBuffers();
@@ -97,7 +122,15 @@ void display()
 
 void init()
 {
+  glEnable(GL_DEPTH_TEST);
   glClearColor(0, 0, 0, 0);
+
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_COLOR_MATERIAL);
+  // glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+  glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+  glShadeModel(GL_SMOOTH);
 }
 
 void reshape(int w, int h)
@@ -114,7 +147,7 @@ void reshape(int w, int h)
 int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(width, height);
   glutInitWindowPosition(100, 100);
   glutCreateWindow("Rotação de Planetas");
