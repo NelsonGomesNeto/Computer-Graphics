d49a3dc7b580d31ff4ef2a68f55d5a57dbc98c22
NelsonGomesNeto <ngn@ic.ufal.br>
2019-04-18 11:19:39 -0300

    Adds BEAUTIFUL Mandel Brot Set visualization

---
+++ playground/Mandel Brot Set/Complex.js
@@ -0,0 +1,24 @@
+class Complex {
+  constructor(real, imaginary) {
+    this.real = real;
+    this.imaginary = imaginary;
+  }
+
+  add(other) {
+    // if ((typeof other) === "object")
+      this.real += other.real, this.imaginary += other.imaginary;
+    // else
+      // this.real += other;
+    return(this);
+  }
+
+  mult(other) {
+    // if ((typeof other) === "object") {
+      let a = this.real, b = this.imaginary, c = other.real, d = other.imaginary;
+      this.real = a*c - b*d, this.imaginary = a*d + b*c;
+    // }
+    // else
+      // this.real *= other;
+    return(this);
+  }
+}
---
+++ playground/Mandel Brot Set/main.js
@@ -0,0 +1,63 @@
+let width = 1000, height = 700;
+var widthCenter, heightCenter;
+let xMin = -2, xMax = 1, yMin = -1.2, yMax = 1.2;
+let xScale, yScale;
+let iterations = 10000;
+
+function setup() {
+  createCanvas(width, height); widthCenter = width * (abs(xMin) / (xMax - xMin)), heightCenter = height * (abs(yMin) / (yMax - yMin));
+  xScale = width / (xMax - xMin), yScale = height / (yMax - yMin);
+}
+
+function f(c, z) {
+  return(z.mult(z).add(c));
+}
+
+function drawGrid() {
+  stroke(255, 0, 0);
+  strokeWeight(1);
+  fill(255, 0, 0);
+  ellipse(0, 0, 5, 5);
+  textSize(15); textAlign(LEFT, BOTTOM)
+  for (var x = xMin; x <= xMax; x += 0.2) {
+    text(x.toFixed(2), x * xScale, 0);
+    line(x * xScale, yMin * yScale, x * xScale, yMax * yScale);
+  }
+  for (var y = yMin; y <= yMax; y += 0.2) {
+    text(y.toFixed(2), 0, y * yScale);
+    line(xMin * xScale, y * yScale, xMax * xScale, y * yScale);
+  }
+}
+
+function drawMandelSteps() {
+  stroke(0, 255, 0);
+  strokeWeight(1);
+  fill(0, 255, 0);
+
+  var z = new Complex(0, 0), c = new Complex((xMax - xMin) * (mouseX - widthCenter) / width, (yMax - yMin) * (mouseY - heightCenter) / height);
+  var points = []
+  for (var i = 0; i < iterations; i ++) {
+    z = f(c, z);
+    // ellipse(z.real * xScale, z.imaginary * yScale, 10 - 5*(i / iterations), 10 - 5*(i / iterations));
+    if (z.real < -1e6 || z.real > 1e6 || z.imaginary < -1e6 || z.imaginary > 1e6) break;
+    points.push([z.real * xScale, z.imaginary * yScale]);
+  }
+
+  noFill();
+  beginShape();
+  curveVertex(points[0][0], points[0][1]);
+  for (var i = 0; i < points.length; i ++) {
+    curveVertex(points[i][0], points[i][1]);
+  }
+  curveVertex(points[points.length - 1][0], points[points.length - 1][1]);
+  endShape();
+}
+
+function draw() {
+  background(0, 0, 255);
+  translate(widthCenter, heightCenter);
+
+  drawGrid();
+
+  drawMandelSteps();
+}
